75dbd92216efffe063ac29ad8f34a8924a5d52e9
dsabdrashitov <dsabdrashitov@github.com>
2024-12-22 08:49:04 +0300

    Make day22p2 monkeys time faster.

--- day22p2/main.go
+++ day22p2/main.go
@@ -51,12 +51,14 @@ func solve(s []int) int {
 		}
 	}
 
-	first := make(map[[5]int]int)
-	for m, dm := range d {
+	firstSum := make(map[[4]int]int)
+	for _, dm := range d {
+		first := make(map[[4]int]bool)
 		for i := range len(dm) - 4 {
-			k := [5]int{m, dm[i+1] - dm[i], dm[i+2] - dm[i+1], dm[i+3] - dm[i+2], dm[i+4] - dm[i+3]}
+			k := [4]int{dm[i+1] - dm[i], dm[i+2] - dm[i+1], dm[i+3] - dm[i+2], dm[i+4] - dm[i+3]}
 			if _, ok := first[k]; !ok {
-				first[k] = dm[i+4]
+				first[k] = true
+				firstSum[k] = firstSum[k] + dm[i+4]
 			}
 		}
 	}
@@ -67,15 +69,10 @@ func solve(s []int) int {
 		for i1 := -9; i1 <= 9; i1++ {
 			for i2 := -9; i2 <= 9; i2++ {
 				for i3 := -9; i3 <= 9; i3++ {
-					sum := 0
-					for m := range d {
-						k := [5]int{m, i0, i1, i2, i3}
-						if val, ok := first[k]; ok {
-							sum += val
-						}
-					}
+					k := [4]int{i0, i1, i2, i3}
+					sum := firstSum[k]
 					if answer < sum {
-						fmt.Println(i0, i1, i2, i3)
+						fmt.Printf("%v, %v, %v, %v: %v\n", i0, i1, i2, i3, sum)
 						answer = sum
 					}
 				}
